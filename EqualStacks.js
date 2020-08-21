737c5ba3babb1acc476a0366e4d0534c856306ea
Shubhankar Gupta <shubhankar.gupta@quovantis.com>
2020-08-21 21:45:37 +0530

    JS Data Structures

---
+++ EqualStacks.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the equalStacks function below.
+ */
+function equalStacks(h1, h2, h3) {
+    let sum = [];
+    let top1 = 0;
+    let top2 = 0;
+    let top3 = 0;
+    
+    sum[0] = h1.reduce((a, b) => a + b, 0);
+    sum[1] = h2.reduce((a, b) => a + b, 0);
+    sum[2] = h3.reduce((a, b) => a + b, 0);
+
+    while(true) {
+        if(h1.length == 0 || h2.length == 0 || h3.length == 0) {
+            return 0;
+        }
+        if(sum[0] == sum[1] && sum[1] == sum[2]) {
+            return sum[0];
+        }
+        if(sum[0] >= sum[1] && sum[0] >= sum[2]) {
+            sum[0] = sum[0] - h1[top1++];
+        } else if(sum[1] >= sum[0] && sum[1] >= sum[2]) {
+            sum[1] = sum[1] - h2[top2++];
+        } else if(sum[2] >= sum[0] && sum[2] >= sum[1]) {
+            sum[2] = sum[2] - h3[top3++];
+    }
+}
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n1N2N3 = readLine().split(' ');
+
+    const n1 = parseInt(n1N2N3[0], 10);
+
+    const n2 = parseInt(n1N2N3[1], 10);
+
+    const n3 = parseInt(n1N2N3[2], 10);
+
+    const h1 = readLine().split(' ').map(h1Temp => parseInt(h1Temp, 10));
+
+    const h2 = readLine().split(' ').map(h2Temp => parseInt(h2Temp, 10));
+
+    const h3 = readLine().split(' ').map(h3Temp => parseInt(h3Temp, 10));
+
+    let result = equalStacks(h1, h2, h3);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
---
+++ SparseArrays.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the matchingStrings function below.
+function matchingStrings(strings, queries) {
+    const count = new Array(queries.length).fill(0);
+    let query = "";
+    for(let i = 0; i < queries.length; i++) {
+        query = queries[i];
+        for(let string of strings) {
+            if(string === query) {
+                count[i]++;
+            }
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    let res = matchingStrings(strings, queries);
+
+    ws.write(res.join("\n") + "\n");
+
+    ws.end();
+}
