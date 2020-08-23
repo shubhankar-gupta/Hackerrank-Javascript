7888afc239624abb9d370855e7b562523fa36c43
shubhankar-gupta <69616334+shubhankar-gupta@users.noreply.github.com>
2020-08-23 16:54:51 +0530

    Update BalancedBrackets.js

--- BalancedBrackets.js
+++ BalancedBrackets.js
@@ -33,11 +33,8 @@ function isBalanced(s) {
     for(let i = 0; i < s.length; i++) {
         if(s[i] == "{" || s[i] == "(" || s[i] == "[") {
             stack.push(s[i]);
-        }
-        else {
-            if(s[i] != bracketsMap.get(stack.pop())) {
+        } else if(s[i] != bracketsMap.get(stack.pop())){
                 return "NO"; 
-            }
         }
     }
 
